bd37737a42485135ed58c3c9a16faeef0d360540
A1Tixonova <igorsivchev@yandex.ru>
2021-11-14 21:19:44 +0300

    merged 06

---
+++ src/apiComponents/spotifyApi/spotifyTemplate.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function SpotifyTemplate() {
+  const [token, setToken] = useState('');
+  const [tracks, setTracks] = useState([]);
+  const id = '06HL4z0CvFAxyc27GXpf02';
+  const market = 'US';
+  let result;
+
+  useEffect(() => {
+    // Api call for retrieving token
+    axios('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization:
+          'Basic ' +
+          new Buffer(
+            'f086e433e8ae4a12ab28cbd7a0cd7581' +
+              ':' +
+              '0092d850e4424787bf19b48f05bed843'
+          ).toString('base64'),
+      },
+      data: 'grant_type=client_credentials',
+    })
+      .then((tokenresponse) => {
+        console.log(tokenresponse.data.access_token);
+        setToken(tokenresponse.data.access_token);
+
+        // Api call for retrieving tracks data
+        axios(
+          `https://api.spotify.com/v1/artists/${id}/top-tracks?market=${market}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Accept: 'application/json',
+              Authorization: 'Bearer ' + tokenresponse.data.access_token,
+            },
+          }
+        )
+          .then((trackresponse) => {
+            console.log(trackresponse.data.tracks);
+            console.log(trackresponse.data.tracks.album);
+            setTracks(trackresponse.data.tracks);
+          })
+          .catch((error) => console.log(error));
+      })
+      .catch((error) => console.log(error));
+  }, []);
+}
+
+export default SpotifyTemplate;
