03108e383c1c628ada0f0536f0cc327abb76bbf3
A1Tixonova <igorsivchev@yandex.ru>
2021-11-20 16:13:09 +0300

    third page smth

--- src/components/Pages/ContentPage3.js
+++ src/components/Pages/ContentPage3.js
@@ -5,8 +5,23 @@ const ContentPage3 = () => {
   return (
     <div className={'main-background'}>
       <div className={'text-content'}>
-        <h1>ContentPage3</h1>
-        <h2></h2>
+        <h1>Some Useful Links!!!</h1>
+        <ul className={'links-border'} style={{ textAlign: 'left' }}>
+          <li>
+            <a href="https://refactoring.guru/ru">Patterns and refactoring</a>
+          </li>
+          <li>
+            <a href="https://github.com/A1Tixonova?tab=repositories">
+              My Personal Github
+            </a>
+          </li>
+          <li>
+            <a href="https://learn.javascript.ru/">JS guide</a>
+          </li>
+          <li>
+            <a href="">Waiting for link...</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
